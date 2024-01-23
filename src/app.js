ff8497ab196b133d74ea8829bbc11d8f8a18d1d5
PAPPU\admin <namangoel55@gmail.com>
2024-01-23 19:39:20 +0530

    added login/register routes and jwt tokenization

--- src/app.js
+++ src/app.js
@@ -16,12 +16,18 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 
 //Health Api
-app.get('/api/v1/health', (_, res)=> {
+app.get("/api/v1/health", (_, res)=> {
     res.status(200).json({
         status: "active",
         service: "Job Listing Backend",
         time: new Date(),
     })
 })
+
+//Importing Routers
+import userRouter from "./routes/user.routes.js";
+
+//Declaring Routes
+app.use("/api/v1/users", userRouter);
 
 export default app;
